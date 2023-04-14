06d784069169227e6513cf5d16373e746621ca07
Darsh <darshshetty04@gmail.com>
2023-04-14 18:54:49 +0530

    Update config.py
    Signed-off-by: Darsh <darshshetty04@gmail.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/45df61ac035ea1f325048.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
